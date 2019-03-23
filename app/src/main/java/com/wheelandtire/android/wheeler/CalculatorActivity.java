b512708d522eedeb9a567a57c58b04a23ffc9868
DESKTOP-S09G547\borislav <botchobanov@gmail.com>
2019-03-23 18:15:35 +0200

    Saturday work on calculator fields and formulas

--- app/src/main/java/com/wheelandtire/android/wheeler/CalculatorActivity.java
+++ app/src/main/java/com/wheelandtire/android/wheeler/CalculatorActivity.java
@@ -3,6 +3,8 @@ package com.wheelandtire.android.wheeler;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -10,21 +12,31 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.wheelandtire.android.wheeler.databinding.ActivityCalculatorBinding;
+import com.wheelandtire.android.wheeler.databinding.ContentCalculatorBinding;
+import com.wheelandtire.android.wheeler.model.Vehicle;
 import com.wheelandtire.android.wheeler.model.VehicleProfile;
+
+import java.util.List;
+
+import retrofit2.Call;
 
 
 public class CalculatorActivity extends AppCompatActivity {
 
     private VehicleViewModel viewModel;
+    private ActivityCalculatorBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calculator);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        setContentView(R.layout.activity_calculator);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_calculator);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(binding.toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle("Calculator");
 
@@ -41,21 +53,22 @@ public class CalculatorActivity extends AppCompatActivity {
 
     private void populateViews(VehicleProfile vehicleProfile) {
 
-        EditText rimWidth = findViewById(R.id.rimWidthView);
-        EditText rimOffset = findViewById(R.id.rimOffsetView);
-        EditText tireWidth = findViewById(R.id.tireWidthView);
-        EditText tireHeight = findViewById(R.id.tireHeightView);
-        EditText tireDiameter = findViewById(R.id.tireDiameterView);
-
         if (vehicleProfile != null) {
             String tireAndRimDiameter = vehicleProfile.getProfileTireAndRimDiameter();
 //            rimDiameter.setText(tireAndRimDiameter);
-            rimWidth.setText(vehicleProfile.getProfileRimWidth());
-            rimOffset.setText(vehicleProfile.getProfileRimOffset());
-            tireWidth.setText(vehicleProfile.getProfileTireWidth());
-            tireHeight.setText(vehicleProfile.getProfileTireHeight());
-            tireDiameter.setText(tireAndRimDiameter);
+            binding.included.rimWidthView.setText(vehicleProfile.getProfileRimWidth());
+            binding.included.rimOffsetView.setText(vehicleProfile.getProfileRimOffset());
+            binding.included.tireWidthView.setText(vehicleProfile.getProfileTireWidth());
+            binding.included.tireHeightView.setText(vehicleProfile.getProfileTireHeight());
+            binding.included.rimDiameterView.setText(tireAndRimDiameter);
         }
+
+        binding.included.goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateRimDiameter();
+            }
+        });
     }
 
     @Override
@@ -79,4 +92,36 @@ public class CalculatorActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private String addUnitOfMeasure(String initalText, boolean isPercentage) {
+        return isPercentage ? initalText.concat("%") : initalText.concat("mm");
+    }
+
+    private int convertInchToMm(EditText currentView) {
+        int currRimDiamInches = Integer.parseInt(currentView.getText().toString());
+        return (int) (currRimDiamInches * 25.4 + .5f);
+    }
+
+    private float calculateDifferencePercentage(int currentSize, int newSize) {
+        float diff = currentSize - newSize;
+        return diff / currentSize * 100;
+    }
+
+    private void calculateRimDiameter() {
+        int currentRimDiameterSize = convertInchToMm(binding.included.rimDiameterView);
+        int newRimDiameterSize = convertInchToMm(binding.included.rimDiameterViewNew);
+        String currentRimDiameterText = addUnitOfMeasure(String.valueOf(currentRimDiameterSize), false);
+        String newRimDiameterText = addUnitOfMeasure(String.valueOf(newRimDiameterSize), false);
+        binding.included.rimDiameterCurrentTv.setText(currentRimDiameterText);
+        binding.included.rimDiameterNewTv.setText(newRimDiameterText);
+
+        int diff = (int) calculateDifferencePercentage(currentRimDiameterSize, newRimDiameterSize);
+        Drawable arrow = diff > 0 ? getDrawable(R.drawable.ic_decrease) : getDrawable(R.drawable.ic_increase);
+
+        String diffRimDiameter = addUnitOfMeasure(String.valueOf(Math.abs(diff)), true);
+        binding.included.rimDiameterDiffTv.setCompoundDrawablesWithIntrinsicBounds(arrow, null, null, null);
+        binding.included.rimDiameterDiffTv.setText(diffRimDiameter);
+    }
+    private void calculateRimWidth() {
+
+    }
 }

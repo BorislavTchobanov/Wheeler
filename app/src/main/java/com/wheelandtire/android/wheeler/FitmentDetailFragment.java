8d6e133aa2ec9a352bac48c8aca0cd360d8db95c
DESKTOP-S09G547\borislav <botchobanov@gmail.com>
2019-03-25 02:05:28 +0200

    Code cleanup FitmentDetailFragment-done

--- app/src/main/java/com/wheelandtire/android/wheeler/FitmentDetailFragment.java
+++ app/src/main/java/com/wheelandtire/android/wheeler/FitmentDetailFragment.java
@@ -1,41 +1,21 @@
 package com.wheelandtire.android.wheeler;
 
 import android.content.Context;
-import android.content.res.Configuration;
-import android.graphics.Bitmap;
-import android.media.MediaMetadataRetriever;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
 import com.wheelandtire.android.wheeler.adapter.FitmentDetailAdapter;
 import com.wheelandtire.android.wheeler.model.Vehicle;
-import com.wheelandtire.android.wheeler.utility.ImageHandler;
-
-import java.net.URLConnection;
-import java.util.HashMap;
-import java.util.Random;
 
 import static com.wheelandtire.android.wheeler.FitmentActivity.EXTRA_VEHICLE;
 
 public class FitmentDetailFragment extends Fragment {
-
-    private boolean mTwoPane;
-    private Vehicle vehicle;
-    private TextView tireTv;
-    private TextView rimTv;
-    private TextView tirePressureTv;
 
     public FitmentDetailFragment() {
     }
@@ -45,29 +25,19 @@ public class FitmentDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fitment_detail_view, container, false);
 
-//        if (getActivity() instanceof FitmentDetailActivity) {
-//            ((FitmentDetailActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        }
-
         assert getArguments() != null;
         if (getArguments().containsKey(EXTRA_VEHICLE)) {
-            vehicle = (Vehicle) getArguments().getSerializable(EXTRA_VEHICLE);
+            Vehicle vehicle = (Vehicle) getArguments().getSerializable(EXTRA_VEHICLE);
             if (vehicle != null) {
-
-
-
                 RecyclerView recyclerView = rootView.findViewById(R.id.fitmentDetailRecyclerView);
 
                 LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
                 recyclerView.setLayoutManager(layoutManager);
 
                 FitmentDetailAdapter fitmentDetailAdapter = new FitmentDetailAdapter(vehicle.getWheels());
-                    recyclerView.setAdapter(fitmentDetailAdapter);
-
-
+                recyclerView.setAdapter(fitmentDetailAdapter);
             }
         }
-
 
         return rootView;
     }
@@ -93,7 +63,6 @@ public class FitmentDetailFragment extends Fragment {
 //            throw new ClassCastException(context.toString() + " must implement OnStepNavButtonClickListener");
 //        }
     }
-
 
 
 }
